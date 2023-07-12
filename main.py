60dcb0e87b9f098f9c7d2fefce7c3c76e170525f
Powerlax <raovishruth@gmail.com>
2023-07-12 13:11:51 -0700

    added predictions, data augmentation, and a dropout layer

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import tensorflow_datasets as tfds
 import pathlib
 
 #get the dataset
-data = tf.keras.utils.get_file('Images', origin='http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar', untar=True)
+#data = tf.keras.utils.get_file('Images', origin='http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar', untar=True)
+data = tf.keras.utils.get_file('flower_photos', origin="https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz", untar=True)
 data = pathlib.Path(data)
 tds = tf.keras.utils.image_dataset_from_directory(directory=data, validation_split=0.2, subset='training', seed=123, image_size=(180,180))
 vds = tf.keras.utils.image_dataset_from_directory(directory=data, validation_split=0.2, subset='validation',seed=123, image_size=(180,180))
@@ -18,8 +19,17 @@ tuner = tf.data.AUTOTUNE
 tds = tds.cache().shuffle(1000).prefetch(buffer_size=tuner)
 vds = vds.cache().prefetch(buffer_size=tuner)
 
+#add data augmentation for randomness
+data_aug = tf.keras.Sequential([
+    tf.keras.layers.RandomFlip("horizontal", input_shape=(180, 180,3)),
+    tf.keras.layers.RandomRotation(0.1),
+    tf.keras.layers.RandomZoom(0.1),
+  ]
+)
+
 #build model
 model = tf.keras.Sequential([
+    data_aug,
     tf.keras.layers.Rescaling(scale=1./255, input_shape=(180,180,3)),   #to rescale the 0, 255 value range down to 0, 1
     tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu'),   #first cnn with relu activiation function
     tf.keras.layers.MaxPooling2D(), 
@@ -27,6 +37,7 @@ model = tf.keras.Sequential([
     tf.keras.layers.MaxPooling2D(), 
     tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
     tf.keras.layers.MaxPooling2D(), 
+    tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(128, activation='relu'),                      #Standard Dense NN
     tf.keras.layers.Dense(num)                                          #one neuron for each class
@@ -39,40 +50,22 @@ model.compile(optimizer='adam', loss=tf.losses.SparseCategoricalCrossentropy(fro
 model.summary()
 
 #training
-training_history=model.fit(tds, validation_data=vds, epochs=10)
+history=model.fit(tds, validation_data=vds, epochs=15)
 
+#predict
+sunflower_url = "https://www.highmowingseeds.com/media/catalog/product/cache/6cbdb003cf4aae33b9be8e6a6cf3d7ad/7/1/7111-1.jpg"
+sunflower_path = tf.keras.utils.get_file('Red_sunflower', origin=sunflower_url)
 
+img = tf.keras.utils.load_img(
+    sunflower_path, target_size=(180, 180)
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
 
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
 
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence.".format(names[np.argmax(score)], 100 * np.max(score))
+)
 
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-print(data)
-
-train = tfds.load('stanford_dogs', split='test')
-test = tfds.load('stanford_dogs', split='test')
-
-for ex in train.take(4):
-    print(ex)
-'''
-
-
-
-
-
-#My attempt at load a tfds dataset
-#builder = tfds.builder(name='cats_vs_dogs')
-#ds = builder.as_dataset(split='test', as_supervised=True)
-#tds, vds = ds["train"], ds["test"]
-#print(ds)
